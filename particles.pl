3220500960b867daebd26dfa4036d6be83477dd8
milia <mlliarm@yandex.com>
2019-12-04 12:01:08 +0200

    added some particles

---
+++ particles.pl
@@ -0,0 +1,36 @@
+mass(electron, 0.511).
+B(electron, 1).
+Le(electron, 1).
+Lmu(electron, 0).
+Ltau(electron, 0).
+S(electron, 0).
+charge(electron, -1).
+lifetime(electron, 'Stable').
+
+mass(electron_eutrino, 2.8E-6).
+B(electron_neutrino, 0).
+Le(electron_neutrino, 1).
+Lmu(electron_neutrino, 0).
+Ltau(electron_neutrino, 0).
+S(electron_neutrino, 0).
+charge(electron_neutrino, 0).
+lifetime(electron_neutrino,'Stable').
+
+mass(muon, 105.7).
+B(muon, 0).
+Le(muon, 0).
+Lmu(muon, 1).
+Ltau(muon, 0).
+S(muon, 0).
+charge(muon, -1).
+lifetime(muon, 2.19E-6).
+
+mass(muon_neutrino, 3.5E-6).
+B(muon_neutrino, 0).
+Le(muon_neutrino, 0).
+Lmu(muon_neutrino, 1).
+Ltau(muon_neutrino, 0).
+S(muon_neutrino, 0).
+charge(muon_neutrino, 0).
+lifetime(muon_neutrino,'Stable').
+
