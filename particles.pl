1d28dce5a1a701dc0ec145b6b1228eb963d81537
milia <mlliarm@yandex.com>
2019-12-04 12:43:02 +0200

    added tau neutrino

--- particles.pl
+++ particles.pl
@@ -1,3 +1,4 @@
+%LEPTONS
 mass(electron, 0.511).
 baryon_number(electron, 1).
 l_e(electron, 1).
@@ -7,7 +8,7 @@ strangeness(electron, 0).
 charge(electron, -1).
 lifetime(electron, 'Stable').
 
-mass(electron_eutrino, 2.8E-6).
+mass(electron_neutrino, '<2.8E-6').
 baryon_number(electron_neutrino, 0).
 l_e(electron_neutrino, 1).
 l_mu(electron_neutrino, 0).
@@ -25,7 +26,7 @@ strangeness(muon, 0).
 charge(muon, -1).
 lifetime(muon, 2.19E-6).
 
-mass(muon_neutrino, 3.5E-6).
+mass(muon_neutrino, '<3.5E-6').
 baryon_number(muon_neutrino, 0).
 l_e(muon_neutrino, 0).
 l_mu(muon_neutrino, 1).
@@ -33,3 +34,25 @@ l_tau(muon_neutrino, 0).
 strangeness(muon_neutrino, 0).
 charge(muon_neutrino, 0).
 lifetime(muon_neutrino,'Stable').
+
+mass(tau, 1784).
+baryon_number(tau, 0).
+l_e(tau, 0).
+l_mu(tau, 0).
+l_tau(tau, 1).
+strangeness(tau, 0).
+charge(tau, -1).
+lifetime(tau, 3.3E-13).
+
+mass(tau_neutrino, 8.4E-6).
+baryon_number(tau_neutrino, 0).
+l_e(tau_neutrino, 0).
+l_mu(tau_neutrino, 0).
+l_tau(tau_neutrino, 1).
+strangeness(tau_neutrino, 0).
+lifetime(tau_neutrino, 'Stable').
+
+%HADRONS
+%Mesons
+
+
