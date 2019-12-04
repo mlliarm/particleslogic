a1f9765b9c1ee10d5b30a83812daca359fdc0dd7
milia <mlliarm@yandex.com>
2019-12-04 14:14:36 +0200

    added anti-particles, refs #1

--- particles.pl
+++ particles.pl
@@ -8,6 +8,15 @@ strangeness(electron, 0).
 charge(electron, -1).
 lifetime(electron, 'Stable').
 
+mass(positron, 0.511).
+baryon_number(positron, 1).
+l_e(positron, 1).
+l_mu(positron, 0).
+l_tau(positron, 0).
+strangeness(electron, 0).
+charge(positron, 1).
+lifetime(positron, 'Stable').
+
 mass(electron_neutrino, 2.8E-6).
 baryon_number(electron_neutrino, 0).
 l_e(electron_neutrino, 1).
@@ -16,6 +25,15 @@ l_tau(electron_neutrino, 0).
 strangeness(electron_neutrino, 0).
 charge(electron_neutrino, 0).
 lifetime(electron_neutrino,'Stable').
+
+mass(anti_electron_neutrino, 2.8E-6).
+baryon_number(anti_electron_neutrino, 0).
+l_e(anti_electron_neutrino, 1).
+l_mu(anti_electron_neutrino, 0).
+l_tau(anti_electron_neutrino, 0).
+strangeness(anti_electron_neutrino, 0).
+charge(anti_electron_neutrino, 0).
+lifetime(anti_electron_neutrino,'Stable').
 
 mass(muon, 105.7).
 baryon_number(muon, 0).
@@ -26,6 +44,15 @@ strangeness(muon, 0).
 charge(muon, -1).
 lifetime(muon, 2.19E-6).
 
+mass(anti_muon, 105.7).
+baryon_number(anti_muon, 0).
+l_e(anti_muon, 0).
+l_mu(anti_muon, 1).
+l_tau(anti_muon, 0).
+strangeness(anti_muon, 0).
+charge(anti_muon, 1).
+lifetime(anti_muon, 2.19E-6).
+
 mass(muon_neutrino, 3.5E-6).
 baryon_number(muon_neutrino, 0).
 l_e(muon_neutrino, 0).
@@ -34,6 +61,15 @@ l_tau(muon_neutrino, 0).
 strangeness(muon_neutrino, 0).
 charge(muon_neutrino, 0).
 lifetime(muon_neutrino,'Stable').
+
+mass(anti_muon_neutrino, 3.5E-6).
+baryon_number(anti_muon_neutrino, 0).
+l_e(anti_muon_neutrino, 0).
+l_mu(anti_muon_neutrino, 1).
+l_tau(anti_muon_neutrino, 0).
+strangeness(anti_muon_neutrino, 0).
+charge(anti_muon_neutrino, 0).
+lifetime(anti_muon_neutrino,'Stable').
 
 mass(tau, 1784).
 baryon_number(tau, 0).
@@ -44,6 +80,15 @@ strangeness(tau, 0).
 charge(tau, -1).
 lifetime(tau, 3.3E-13).
 
+mass(anti_tau, 1784).
+baryon_number(anti_tau, 0).
+l_e(anti_tau, 0).
+l_mu(anti_tau, 0).
+l_tau(anti_tau, 1).
+strangeness(anti_tau, 0).
+charge(anti_tau, 1).
+lifetime(anti_tau, 3.3E-13).
+
 mass(tau_neutrino, 8.4E-6).
 baryon_number(tau_neutrino, 0).
 l_e(tau_neutrino, 0).
@@ -52,6 +97,15 @@ l_tau(tau_neutrino, 1).
 strangeness(tau_neutrino, 0).
 charge(tau_neutrino, 0).
 lifetime(tau_neutrino, 'Stable').
+
+mass(anti_tau_neutrino, 8.4E-6).
+baryon_number(anti_tau_neutrino, 0).
+l_e(anti_tau_neutrino, 0).
+l_mu(anti_tau_neutrino, 0).
+l_tau(anti_tau_neutrino, 1).
+strangeness(anti_tau_neutrino, 0).
+charge(anti_tau_neutrino, 0).
+lifetime(anti_tau_neutrino, 'Stable').
 
 %HADRONS
 %Mesons
@@ -63,6 +117,15 @@ l_tau(pion, 0).
 strangeness(pion, 0).
 charge(pion, 1).
 lifetime(pion, 2.6E-8).
+
+mass(anti_pion, 139.6).
+baryon_number(anti_pion, 0).
+l_e(anti_pion, 0).
+l_mu(anti_pion, 0).
+l_tau(anti_pion, 0).
+strangeness(anti_pion, 0).
+charge(anti_pion, -1).
+lifetime(anti_pion, 2.6E-8).
 
 mass(pion0, 135.0).
 baryon_number(pion0, 0).
@@ -81,3 +144,12 @@ l_tau(kaon, 0).
 strangeness(kaon, 1).
 charge(kaon, 1).
 lifetime(kaon, 1.24E-8).
+
+mass(anti_kaon, 493.7).
+baryon_number(anti_kaon, 0).
+l_e(anti_kaon, 0).
+l_mu(anti_kaon, 0).
+l_tau(anti_kaon, 0).
+strangeness(anti_kaon, 1).
+charge(anti_kaon, -1).
+lifetime(anti_kaon, 1.24E-8).
