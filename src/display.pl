19229e87b82c2d71dfbb63c779310b95c9bf5267
milia <mlliarm@yandex.com>
2020-04-26 01:57:04 +0200

    minor edit

--- src/display.pl
+++ src/display.pl
@@ -1,10 +1,11 @@
+%Predicate that prints the Name, mass, charge and spin of particle X.
 print_info(X) :-
     write('----------'),
     write('\n'),
     write('Name: '), write(X),
     write('\n'),
     mass(X, Mass),
-    write('Mass: '), write(Mass),write(' Mev/c^2'),
+    write('Mass: '), write(Mass), write(' Mev/c^2'),
     write('\n'),
     charge(X, Charge),
     write('Charge: '), write(Charge),
